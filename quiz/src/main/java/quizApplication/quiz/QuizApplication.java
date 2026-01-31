c6e231874079f902b3e3d0d3d3a34f6a307a2393
AR6921 <AR6921@OGB-BO-HO-38.ogb.com>
2026-01-31 18:39:41 +0530

    minor commit

--- quiz/src/main/java/quizApplication/quiz/QuizApplication.java
+++ quiz/src/main/java/quizApplication/quiz/QuizApplication.java
@@ -2,15 +2,23 @@ package quizApplication.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EntityScan("quizApplication.quiz.entity")
 @SpringBootApplication
-public class QuizApplication {
+public class QuizApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
 	// THIS IS CHANGE
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QuizApplication.class);
+    }
+	
 
 }
